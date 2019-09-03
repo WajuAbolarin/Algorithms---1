d8503ea52485aa9c04d41a87220d1d4209a33892
Abolarin Olanrewaju Olabode <waju.abolarin@gmail.com>
2019-09-03 14:00:47 +0100

    all tests are green

--- __test__/time.spec.js
+++ __test__/time.spec.js
@@ -1,6 +1,8 @@
 import travelTime from "../src/index";
-
 import testData from "./mocks/testData.json";
+import test2 from "./mocks/test2.json";
+import test3 from "./mocks/test3.json";
+import test4 from "./mocks/test4.json";
 
 describe("Time spent", () => {
   test("It returns a string", () => {
@@ -8,5 +10,15 @@ describe("Time spent", () => {
   });
   test("It returns sum of durations", () => {
     expect(travelTime(testData)).toEqual("21 hours, 55 minutes");
+  });
+  test("It returns sum of durations with hours missing in some", () => {
+    expect(travelTime(test2)).toEqual("10 hours");
+  });
+
+  test("It returns sum of durations with minutes missing in some", () => {
+    expect(travelTime(test3)).toEqual("9 hours");
+  });
+  test("It returns sum of durations with weird data", () => {
+    expect(travelTime(test4)).toEqual("25 minutes");
   });
 });
--- src/index.js
+++ src/index.js
@@ -7,12 +7,16 @@ export default function travelTime(tripData) {
     return result;
   };
   const prop = key => obj => obj[key];
-  const Regex = /\d+/g;
+  const numRegex = /\d+/g;
+  const hrRegex = /\d+\s+?[hH]/g;
+  const mntRegex = /\d+\s+?[mM]/g;
   const toLowerCase = s => String.prototype.toLocaleLowerCase.call(s);
   const split = sep => str => String.prototype.split.call(str, sep);
-  const match = reg => str => String.prototype.match.call(str, reg)[0];
+  const match = reg => str => String.prototype.match.call(str, reg);
+  const firstMatch = matches => (Array.isArray(matches) ? head[matches] : "");
   const trim = str => String.prototype.trim.call(str);
   const gte = limit => n => n >= limit;
+  const gt = limit => n => n > limit;
   const head = arr => arr[0];
   const tail = arr => arr[arr.length - 1];
   const reduce = fn => arr => Array.prototype.reduce.call(arr, fn, 0);
@@ -20,36 +24,46 @@ export default function travelTime(tripData) {
   const eq = limit => n => n === limit;
   const arraySum = reduce(sum);
   const greaterThan1 = gte(1);
-  const equal1 = eq(1);
+  const greaterThan0 = gt(0);
+  const equal0 = eq(0);
+  const pluralize = (string, n) => (n > 1 ? `${string}s` : `${string}`);
+  const isValue = x => !!x;
+  const filter = predicate => arr => arr.filter(predicate);
 
   const getTrips = prop("trips");
   const getDurations = map(prop("duration"));
   const splitParts = split(",");
-  const pickNumbers = match(Regex);
-  const getHours = map(head);
-  const getMins = map(tail);
+  const pickNumbers = match(numRegex);
+  const getHours = match(hrRegex);
+  const getMins = match(mntRegex);
   const sumNumbers = reduce(sum);
   const toMinutes = h => Number(h) * 60;
   const toTimeString = mins => {
     let hrs = Math.floor(mins / 60);
     let minutes = mins % 60;
 
-    return `${greaterThan1(hrs) ? hrs : ""} ${
-      equal1(hrs) ? "hour" : "hours"
-    }, ${greaterThan1(minutes) ? minutes : ""} ${
-      equal1(minutes) ? "minute" : "minutes"
-    }`;
+    let hrString = greaterThan0(hrs) ? `${hrs} ${pluralize("hour", hrs)}` : "";
+
+    let mntString = greaterThan0(minutes)
+      ? `${minutes} ${pluralize("minute", minutes)}`
+      : "";
+
+    return trim(`${hrString} ${mntString}`).replace(/(hours?)\s/, "$1, ");
   };
 
   const trips = getTrips(tripData);
   const durations = getDurations(trips);
   const lowerCase = map(toLowerCase);
   const durationsInLowerCase = lowerCase(durations);
-  const hours = map(trim)(getHours(durationsInLowerCase.map(splitParts)));
-  const minutes = map(trim)(getMins(durationsInLowerCase.map(splitParts)));
+  const hours = map(pickNumbers)(
+    filter(isValue)(map(getHours)(durationsInLowerCase))
+  );
+  const minutes = map(pickNumbers)(
+    filter(isValue)(map(trim)(getMins(durationsInLowerCase.map(splitParts))))
+  );
+
   const hrsInMinutes = arraySum(map(toMinutes)(map(pickNumbers)(hours)));
   const mins = arraySum(map(pickNumbers)(minutes));
   const totalMins = sum(hrsInMinutes, mins);
-
-  return toTimeString(totalMins);
+  return toTimeString(trim(totalMins));
 }
